bb8cc7189f46cc1b4943f6d73d3c28c23524ebe1
moiz-naeem <moiznaeem20@gmail.com>
2024-05-26 12:28:23 +0300

    Added more methods to StudentCourse

--- tracker/StudentCourse.java
+++ tracker/StudentCourse.java
@@ -1,4 +1,9 @@
-package dev.m3s.programming2.homework2;
+package dev.m3s.programming2.homework3;
+import java.time.Year;
+
+//the last part of setGrade regarding setting year needs to be checked 
+//isNumericGrade also need to be checked in course.java
+
 public class StudentCourse {
 
    private Course course;
@@ -29,11 +34,13 @@ public class StudentCourse {
    }
    
    protected void setGrade(int gradeNum){
+      if(Character.isLowerCase(gradeNum)){
+         Character.toUpperCase(gradeNum);
+      }
       if (course.isNumericGrade()){
          if (checkGradeValidity(gradeNum)){
             if (gradeNum != 'A' && gradeNum != 'F'){
                this.gradeNum = gradeNum;
-            }
          }
       }
       if (!course.isNumericGrade()){
@@ -41,18 +48,15 @@ public class StudentCourse {
             if (gradeNum == 'A'|| gradeNum == 'F'){
                this.gradeNum = gradeNum;
             }
-            else if (gradeNum == 'a'){
-               this.gradeNum = 'A';
-            }
-            else if (gradeNum == 'f'){
-               this.gradeNum = 'F';
-            }
          }
       }
-      if (yearCompleted < 2000){
-         yearCompleted = ConstantValues.CURRENT_YEAR;
+      if (yearCompleted > 2000 && yearCompleted <= Year.now().getValue()){
+         setYear(yearCompleted);
       }
-   }
+      else{
+         this.yearCompleted =  Year.now().getValue();
+      }
+      } }
    
    private boolean checkGradeValidity(final int gradeNum){
       if (gradeNum >= 0 && gradeNum <= 5){
@@ -65,10 +69,10 @@ public class StudentCourse {
    }
    
    public boolean isPassed(){
-      if (gradeNum > 0  || gradeNum == 'A'){
-         return true;
+      if (gradeNum == ConstantValues.MIN_GRADE || gradeNum == ConstantValues.GRADE_FAILED){
+         return false;
       }
-      return false;
+      return true;
    }
    
    public int getYear(){
@@ -76,7 +80,7 @@ public class StudentCourse {
    }
    
    public void setYear(final int year){
-      if (year > 2000 && year <= ConstantValues.CURRENT_YEAR){
+      if (year > 2000 && year <= Year.now().getValue()){
          yearCompleted = year;
       }
    }
@@ -85,11 +89,12 @@ public class StudentCourse {
         
         String finalStr = course.toString() + " Year: " +  getYear() + ", " + "Grade: " ;
         if(getGradeNum() == 0){
-         return finalStr + "Not graded" + ".]";
+         return finalStr + "Not graded" + ".] \n";
       }
         else{
-         return finalStr + getGradeNum() +".]";
+         return finalStr + getGradeNum() +".] \n";
       }
    }
    
+
 }
